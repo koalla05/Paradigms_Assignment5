04a81726b609517848af08c9eb238705d1d16e62
koalla05 <kov.koalla@gmail.com>
2024-07-14 00:40:36 +0300

    Tokenization
    Tokenization for numbers, white spaces and symbols

---
+++ cmake-build-debug/CMakeFiles/Paradigms_Assignment5.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Paradigms_Assignment5.dir/main.cpp.obj"
+  "CMakeFiles/Paradigms_Assignment5.dir/main.cpp.obj.d"
+  "Paradigms_Assignment5.exe"
+  "Paradigms_Assignment5.exe.manifest"
+  "Paradigms_Assignment5.pdb"
+  "libParadigms_Assignment5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Paradigms_Assignment5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,7 +1,65 @@
 #include <iostream>
+#include <string>
+#include <cctype> // for isxdigit and isspace
+#include <string.h>
+#include <vector>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello, World!" << std::endl;
+class Token {
+public:
+    vector<std::string> array;
+
+    void append(const std::string& b) {
+        array.push_back(b);
+    }
+
+    string getTokens() const {
+        string result;
+        for (const auto& token : array) {
+            result += token;
+        }
+        return result;
+    }
+};
+
+vector<string> Tokenization(const string& input) {
+    Token tokens;
+    string b = "";
+    for (size_t i = 0; i < input.length(); i++) {
+        char ch = input[i];
+
+        if (isxdigit(ch)) {
+            b += ch;
+        } else if (isspace(ch)) {
+            if (!b.empty() && b != "-") {
+                tokens.append(b);
+                b.clear();
+            }
+        } else {
+            if (!b.empty() && b != "-") {
+                tokens.append(b);
+                b.clear();
+            }
+            tokens.append(string(1, ch)); // Convert char to string and append
+        }
+    }
+
+    if (!b.empty()) tokens.append(b);
+    return tokens.array;
+}
+
+void output(const vector<string>& input) {
+    cout << "Tokenization:" << endl;
+    for (int i = 0; i < input.size(); i++) {
+        cout << input[i] << endl;
+    }
+}
+
+int main() {
+    string input;
+    cout << "Enter: " << endl;
+    getline(cin, input);
+    vector<string> result = Tokenization(input);
+    output(result);
     return 0;
 }
